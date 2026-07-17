978c1efd38d192b5020c8cd29fc4898112fe11dd
HelenWeiss <elena.kherson@gmail.com>
2026-07-17 15:45:27 +0300

    Package gross_calculator, Class GrossPayCalculator: - modifying Variables (Scanner); - arithmetic Operators. Class Blank, Method fillInTheBlank.

---
+++ src/gross_calculator/Blank.java
@@ -0,0 +1,11 @@
+package gross_calculator;
+
+public class Blank {
+
+    static String fillInTheBlank(String season, int numberOfCups, String adjective) {
+        String story = "On a " + adjective + " " + season + " day, I drink a minimum of "
+                + numberOfCups + " cups of coffee."; // TODO: Your code goes here.
+
+        return story;
+    }
+}
--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,21 +1,40 @@
 package gross_calculator;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
 
     static String message = "default message";
 
     public static void main (String[] args) {
 
+        // Practice: class GrossPayCalculator;
+
         // 1. Get the number of hours worked
-        int hours = 40;
+        int hours = 0;
+        System.out.println("How many hours did you work?");
+
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
 
         // 2. Get the hourly pay rate
-        double payRate = 25.50;
+        double payRate = 0;
+        System.out.println("What is your pay rate?");
+
+        payRate = scanner.nextDouble();
+        scanner.close();
 
         // 3. Multiply hours and pay rate
         double grossPay = hours * payRate;
 
         // 4. Display result
         System.out.println("Gross pay: " + grossPay);
+
+        // Practice: class Blank;
+/*
+        Blank.fillInTheBlank("winter", 4, "cold");
+        Blank.fillInTheBlank("summer", 0, "rainy");
+ */
+
     }
 }
